35ca2ab819c361773f897557ceb9ee998657fd96
Saderty <sbk1162011@mail.ru>
2020-02-27 18:20:59 +0300

    Upd

--- src/main/java/ShopTable.java
+++ src/main/java/ShopTable.java
@@ -1,40 +1,50 @@
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ShopTable extends JFrame {
     DefaultTableModel model;
+    JTable table;
 
     ShopTable() {
         setSize(1000, 1000);
         model = new DefaultTableModel();
-        final JTable table = new JTable(model);
+        table = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(table);
-        //table.setBounds(0, 0, 500, 500);
         scrollPane.setBounds(0, 0, 500, 500);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         JButton rowButton = new JButton();
         rowButton.setBounds(0, 500, 500, 50);
-        rowButton.setText("Add Row");
+        rowButton.setText("Добавить строку");
         add(rowButton);
         JButton calcButton = new JButton();
         calcButton.setBounds(0, 550, 500, 50);
-        calcButton.setText("Calc Table");
+        calcButton.setText("Рассчитать таблицу");
         add(calcButton);
+        JButton printButton = new JButton();
+        printButton.setBounds(0, 600, 500, 50);
+        printButton.setText("Создать таблицу");
+        add(printButton);
+
         add(scrollPane);
 
-        model.addColumn("Name");
-        model.addColumn("Price");
-        model.addColumn("Number");
-        model.addColumn("Result");
+        model.addColumn("Название");//0
+        model.addColumn("Кол-во");//1
+        model.addColumn("Цена");//2
+        model.addColumn("%");//3
+        model.addColumn("Цена + %");//4
+        model.addColumn("Итого");//5
+        model.addColumn("Итого + %");//6
 
-        String[] strings = {"11", "22", "33", "44"};
-        model.addRow(strings);
-        model.addRow(strings);
-        model.addRow(strings);
-        model.addRow(new String[]{"Total"});
+        model.addRow(new String[]{"Результат"});
 
 //        ListSelectionModel selectionModel = table.getSelectionModel();
 //        selectionModel.addListSelectionListener(new ListSelectionListener() {
@@ -68,13 +78,28 @@ public class ShopTable extends JFrame {
                 double a;
                 double b;
                 double c = 0;
+                double p = 0;
                 for (int i = 0; i < table.getRowCount() - 1; i++) {
                     a = Double.parseDouble((String) table.getValueAt(i, 1));
                     b = Double.parseDouble((String) table.getValueAt(i, 2));
-                    table.setValueAt(a * b, i, 3);
+                    p = Double.parseDouble((String) table.getValueAt(i, 3)) / 100;
+                    table.setValueAt(b + b * p, i, 4);
+                    table.setValueAt(a * b, i, 5);
                     c += a * b;
+                    table.setValueAt(a * b + a * b * p, i, 6);
                 }
-                table.setValueAt(c, table.getRowCount()-1, 3);
+                for (int i = 1; i < table.getColumnCount(); i++) {
+                    table.setValueAt("", table.getRowCount() - 1, i);
+                }
+                table.setValueAt(c, table.getRowCount() - 1, 5);
+                table.setValueAt(c + c * p, table.getRowCount() - 1, 6);
+            }
+        });
+
+        printButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                toExcel();
             }
         });
 
@@ -83,5 +108,26 @@ public class ShopTable extends JFrame {
 
     public static void main(String[] args) {
         new ShopTable();
+    }
+
+    void toExcel() {
+        try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+            XSSFSheet sheet = workbook.createSheet("Page 1");
+            XSSFRow row = sheet.createRow(0);
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                row.createCell(i).setCellValue(table.getColumnName(i));
+            }
+            for (int i = 0; i < table.getRowCount(); i++) {
+                row = sheet.createRow(i + 1);
+                for (int j = 0; j < table.getColumnCount(); j++) {
+                    String s = String.valueOf(table.getValueAt(i, j));
+                    if (s == null) s = "";
+                    row.createCell(j).setCellValue(s);
+                }
+            }
+            workbook.write(new FileOutputStream("/home/saderty/Table.xlsx"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
